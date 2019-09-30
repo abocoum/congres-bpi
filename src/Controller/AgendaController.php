b7c374ae89e5a295b7273d55f2a6d375d8342f4a
Mohamed Maiga <mohamed.maiga@ynov.com>
2019-09-30 15:54:37 +0000

    texte contextuel

--- src/Controller/AgendaController.php
+++ src/Controller/AgendaController.php
@@ -18,11 +18,31 @@ class AgendaController extends AbstractController
     }
 
     /**
-     * @Route("/agenda-details/{_locale}", name="details", defaults={"_locale": "fr"}, requirements={"_locale": "fr|en"})
+     * @Route("/agenda-details-14-novembre-2019/{_locale}", name="details_1", defaults={"_locale": "fr"}, requirements={"_locale": "fr|en"})
      */
-    public function details()
+    public function details_1()
     {
-        return $this->render('agenda/details.html.twig', [
+        return $this->render('agenda/details_1.html.twig', [
+            'controller_name' => 'AgendaController',
+        ]);
+    }
+
+    /**
+     * @Route("/agenda-details-15-novembre-2019/{_locale}", name="details_2", defaults={"_locale": "fr"}, requirements={"_locale": "fr|en"})
+     */
+    public function details_2()
+    {
+        return $this->render('agenda/details_2.html.twig', [
+            'controller_name' => 'AgendaController',
+        ]);
+    }
+
+    /**
+     * @Route("/agenda-details-16-novembre-2019/{_locale}", name="details_3", defaults={"_locale": "fr"}, requirements={"_locale": "fr|en"})
+     */
+    public function details_3()
+    {
+        return $this->render('agenda/details_3.html.twig', [
             'controller_name' => 'AgendaController',
         ]);
     }
